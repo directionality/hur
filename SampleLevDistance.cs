b79a9ff7dd798f5e63b841d7ce2215a3e6adcc6e
Muntaser Syed <jemsbhai@gmail.com>
2017-11-11 10:36:30 -0500

    sample levenshtein distance for 2 strings
    simple distance for 2 strings using minedit

---
+++ SampleLevDistance.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+
+
+        public static int LevDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LevDistance("12441", "21545"));
+            Console.WriteLine(LevDistance("12114", "12115"));
+            Console.WriteLine(LevDistance("212121", "221121"));
+
+            Console.ReadKey();
+        }
+    }
+}
